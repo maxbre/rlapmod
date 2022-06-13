1f0a4b7d68b6d61cb2442038afe94e2a72c63f11
maxbre <maxbre67@gmail.com>
2022-06-13 08:26:32 +0200

    add logo

---
+++ inst/extdata/sticker/my_sticker_rlapmod.R
@@ -0,0 +1,35 @@
+# https://github.com/GuangchuangYu/hexSticker
+library(hexSticker)
+library(ggplot2)
+
+p<-ggplot(aes(x = mpg, y = wt), data = mtcars) +
+  geom_point(aes(colour = factor(cyl)), show.legend = FALSE)+
+  scale_color_viridis_d(option = "plasma")+
+  theme_void() +
+  theme_transparent()
+
+p
+
+s<-sticker(p, 
+           package = "rlapmod", 
+           p_color = "#000080",
+           p_size = 20, 
+           p_x = 1,
+           p_y = 1.5,
+           s_x = 0.9,
+           s_y = 0.8, 
+           s_width = 1, 
+           s_height = 1,
+           spotlight = TRUE,
+           l_width = 1.1,
+           l_height = 1.1,
+           l_alpha = 0.6,
+           l_x = 0.48,
+           l_y = 1.0,
+           h_fill = "#5DB5F8",
+           h_color = "#000080",
+           filename="./rlapmod.png")
+
+plot(s)
+
+
